e09c5fc47a0755c331c86df4b740a7fd7dd1cd06
OHoltan <96868594+OHoltan@users.noreply.github.com>
2023-03-10 11:37:46 +0100

    first major bit of JS

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const doc = document.querySelector('body')
+const doc = document.querySelector('.game');
 
-const button = document.querySelector('button')
+const button = document.querySelector('button');
 
-document.addEventListener("click", function(e) {
-    console.log(e.target.id)
+let arrOfButtons = [];
+
+let start = false
+
+doc.addEventListener("click", function(e) {
+    // console.log(e.target.id)
+    if (e.target.id == "button1" || e.target.id == "button2" || e.target.id == "button3" || e.target.id == "button4") {
+        console.log("element was pressed")
+
+    }
 })
+
+
+// compeare both arrays, each turn and add a score and run randButton()
+
+
+
+
+function randButton() {
+    const buttons = ['button1', 'button2', 'button3', 'button4'];
+    const randomIndex = Math.floor(Math.random() * buttons.length)
+    // return buttons[randomIndex]
+    arrOfButtons.push(buttons[randomIndex])
+}
